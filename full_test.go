b837bfd1ba3aa375ed2428f533ef711800784882
MacRat <m@crat.jp>
2021-12-30 00:21:23 +0900

    test: add test cases of full-test

--- full_test.go
+++ full_test.go
@@ -23,7 +23,7 @@ func Test_fulltest(t *testing.T) {
 		t.Fatalf("failed to get current user information: %s", err)
 	}
 
-	tmpl, err := template.ParseGlob("testdata/cron.d/*")
+	tmpls, err := template.ParseGlob("testdata/cron.d/*")
 	if err != nil {
 		t.Fatalf("failed to read crontab template: %s", err)
 	}
@@ -38,18 +38,29 @@ func Test_fulltest(t *testing.T) {
 		}
 	}
 
-	f, err := os.Create(filepath.Join(cronPath, "fulltest"))
-	if err != nil {
-		t.Fatalf("failed to create crontab: %s", err)
+	for _, tmpl := range tmpls.Templates() {
+		f, err := os.Create(filepath.Join(cronPath, tmpl.Name()))
+		if err != nil {
+			t.Fatalf("failed to create crontab: %s", err)
+		}
+
+		err = tmpl.ExecuteTemplate(f, tmpl.Name(), map[string]string{
+			"User":       u.Username,
+			"CronPath":   cronPath,
+			"OutputPath": outputPath,
+		})
+		if err != nil {
+			t.Fatalf("failed to generate crontab: %s", err)
+		}
 	}
 
-	err = tmpl.ExecuteTemplate(f, "fulltest", map[string]string{
-		"User":       u.Username,
-		"CronPath":   cronPath,
-		"OutputPath": outputPath,
-	})
-	if err != nil {
-		t.Fatalf("failed to generate crontab: %s", err)
+	// wait 20sec if current second is under 10.
+	if curSec := time.Now().Second(); curSec < 10 {
+		time.Sleep(20 * time.Second)
+	}
+	// wait 30sec if current second is over 50.
+	if curSec := time.Now().Second(); curSec > 50 {
+		time.Sleep(30 * time.Second)
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute+10*time.Second)
@@ -61,13 +72,17 @@ func Test_fulltest(t *testing.T) {
 		"CONCRON_PATH=" + cronPath,
 	})
 
-	readTimingOutput := func(name string) (string, int, int) {
+	readFile := func(name string) string {
 		raw, err := os.ReadFile(filepath.Join(outputPath, name))
 		if err != nil {
 			t.Errorf("failed to read %s: %s", name, err)
-			return "", 0, 0
+			return ""
 		}
-		xs := strings.Split(strings.Trim(string(raw), "\n"), ":")
+		return string(raw)
+	}
+
+	readTimingOutput := func(name string) (string, int, int) {
+		xs := strings.Split(strings.Trim(readFile(name), "\n"), ":")
 		m, err := strconv.Atoi(xs[1])
 		if err != nil {
 			t.Errorf("failed to parse minute part of %s: %s", name, err)
@@ -111,5 +126,15 @@ func Test_fulltest(t *testing.T) {
 		t.Errorf("unexpected minute: expected %d but got %d", curMin, m)
 	} else if s != 0 {
 		t.Errorf("unexpected second: expected %d but got %d", 0, s)
+	}
+
+	for _, tt := range [][]string{
+		{"count3", strings.Repeat("hello world\n", 3)},
+		{"count2", strings.Repeat("hello world\n", 2)},
+		{"count1", strings.Repeat("hello world\n", 1)},
+	} {
+		if output := readFile(tt[0]); output != tt[1] {
+			t.Errorf("expected %q but got %q", tt[1], output)
+		}
 	}
 }
