b6259eddda453b300853499b936f9fd4fdb86555
MacRat <m@crat.jp>
2022-01-08 18:50:42 +0900

    chore(installer): fix install script

--- assets/install.sh
+++ assets/install.sh
@@ -3,12 +3,12 @@
 set -e
 
 latest_version() {
-    curl -L -s -H 'Accept: application/json' https://github.com/macrat/concron/releases/latest | sed -e 's/.*"tag_name":"v\([-+.a-zA-Z0-9]*\)".*/\1/'
+    curl -sLf -H 'Accept: application/json' https://github.com/macrat/concron/releases/latest | sed -e 's/.*"tag_name":"v\([-+.a-zA-Z0-9]*\)".*/\1/'
 }
 
 version=${CONCRON_VERSION:-$(latest_version)}
 
-download_url="https://github.com/macrat/concron/releases/download/${version}/concron_${version}_$(uname -s | tr '[:upper:]' '[:lower:]')_$(uname -m)"
+download_url="https://github.com/macrat/concron/releases/download/v${version}/concron_${version}_$(uname -s | tr '[:upper:]' '[:lower:]')_$(uname -m)"
 
 
 echo "Start install Concron version ${version}..."
@@ -18,7 +18,7 @@ echo "Arch: $(uname -m)"
 echo "URL: ${download_url}"
 echo ""
 
-curl -sSLf ${download_url} -o /usr/local/bin/concron
+curl -SLf ${download_url} -o /usr/local/bin/concron
 chmod 755 /usr/local/bin/concron
 
 echo ""
